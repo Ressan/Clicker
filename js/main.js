cf3ef0c72a8b07125ece954d4f286f169546af4e
Ressan <46036784+Ressan@users.noreply.github.com>
2022-03-12 02:31:33 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@ console.log('Hello');
 console.error('World');
 console.warn('!');
 
+
+let minute = 0;
+let second = 29;
+let millisecond = 1000;
+let hour = 0;
+
+let cron;
+
 let cpt = 0;
-let chronos = 10;
 let firstclick = true;
 let play = true;
 let longueur = window.outerHeight;
@@ -11,7 +18,7 @@ let largeur = window.outerWidth;
 console.log("h:",longueur,"w:",largeur);
 
 
-let timer = document.querySelector("#timer");
+//let timer = document.querySelector("#timer");
 /*
 let timer = document.querySelector(".timer");
 */
@@ -31,31 +38,82 @@ boxes.forEach(box => {
 box2.addEventListener('click', (event) =>{
 
     if (play) {        
-        if (firstclick) {    
-            console.warn("Oh le click!");
-            cpt +=1;
-            let nx = Math.floor((Math.random() * (largeur))-125);
-            let ny = Math.floor((Math.random() * (longueur))-125);
-            
-            //for(i = box2.get)
-            event.target.style.top = nx + "px";
-            event.target.style.left = ny + "px";
-            
-            console.log("click box 2:",cpt,"\nNew Coo: x",nx,"y",ny);
-        }
+        if (firstclick) {
+            start();
+            firstclick =false;
+        }    
+        console.warn("Oh le click!");
+        cpt +=1;
+        let nx = Math.floor((Math.random() * (500-125))+125);
+        let ny = Math.floor((Math.random() * (1000-125))+125);
+        
+        event.target.style.top = nx + "px";
+        event.target.style.left = ny + "px";
+        console.log("click box 2:",cpt,"\nNew Coo: x",nx,"y",ny);
+        document.getElementById('nbClick').innerText = returnData(cpt);
     }
 
 
 })
 
-function setTime(){
-    if(play) {
-        if(firstclick)
-        { timer.style.textShadow = "0px 0px 10px whitesmoke";  }
-        chronos--;
-        if(chronos <=0)
-        {play = false;}
-        document.querySelector('.timer')
-    }
+
+
+const rejouer = () => {
+    //On rejoue (en rafraichissant la page)
+    document.location.reload(false);
 }
 
+function pause() {
+    clearInterval(cron);
+}
+
+function start() {
+    pause();
+    cron = setInterval(() => { timer(); }, 10);
+}
+
+function timer() {
+    if ((millisecond -= 10) == 0 && second != 0) {
+        millisecond = 1000;
+        second--;
+    }
+    if (second == 0 && minute != 0) {
+        second = 60;
+        minute--;
+    }
+    if (minute == 00 && hour!= 0) {
+        minute = 60;
+        hour--;
+    }
+
+    if(endTimer() == true)
+    {
+        console.log('EndTimer');
+        pause();
+        play = false;
+        console.log('test');
+        document.querySelector("#minute").style.color = "orangered";
+        document.querySelector("#second").style.color = "orangered";
+        document.querySelector("#millisecond").style.color = "orangered";
+        document.querySelector('#compteur').style.color = "turquoise";
+        document.querySelector('#compteur').style.fontSize = "30px";
+    }
+
+    document.getElementById('minute').innerText = returnData(minute);
+    document.getElementById('second').innerText = returnData(second);
+    document.getElementById('millisecond').innerText = returnData(millisecond);
+}
+
+function endTimer()
+{
+    let fin = false;
+    if(hour == 0 && minute == 0 && second == 0 && millisecond == 0)
+    {
+        fin = true;
+    }
+    return fin;
+}
+
+function returnData(input) {
+    return input > 10 ? input : `${input}`
+}
